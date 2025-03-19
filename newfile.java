a2202dd87d3b40381b5426785b189f5ab1dcb425
Christian-Santiago0710 <>
2025-03-19 12:17:55 +0800

    Last Sync: 2025-03-19 04:17:55 (Mobile)

---
+++ newfile.java
@@ -0,0 +1,11 @@
+public class PrintDataTypes {
+    public static void main(String[] args) {
+        int intValue = 10;
+        double doubleValue = 3.14;
+        String stringValue = "Hello!";
+
+        System.out.println("Integer value: " + intValue);
+        System.out.println("Double value: " + doubleValue);
+        System.out.println("String value: " + stringValue);
+    }
+}
